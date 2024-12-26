4ec663f738ab6171caa2ab62eae356ada4284171
fujimotoayuki <ryuheigg@icloud.com>
2024-12-26 15:20:55 +0900

    readmeファイルの記述

---
+++ src/test/java/com/example/varioustask/VarioustaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.varioustask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarioustaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
